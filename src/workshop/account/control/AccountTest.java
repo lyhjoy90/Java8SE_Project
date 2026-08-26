4933ad6a2636ca7d51c334035d4d4ed496ba20b5
lyhjoy90 <lyhjoy90@gmail.com>
2026-08-26 10:25:56 +0900

    8월25일수업놓친부분8월26일오전10시완료

---
+++ src/workshop/account/control/AccountTest.java
@@ -0,0 +1,32 @@
+package workshop.account.control;
+import workshop.account.entity.Account;
+import workshop.account.exception.InsufficientBalanceException;
+//ctrl+f11 java run
+public class AccountTest {
+	public static void main(String[] args) {
+		//try - catch 구문
+		try {
+			Account account2 = new Account("A1200","221-22-3488",200000);
+			//getClass().getName() + '@' + Integer.toHexString(hashCode())
+			//출금
+			account2.withdraw(500000);
+			System.out.println(account2.toString());
+			
+			Account account = new Account() ;
+			
+			//setter 매서드를 호출하여 Account 변수의 값을 변경 요청
+			//고객번호 : "A1100", 계좌번호 : "221-22-3477", 잔액 : 100000
+			account.setCustId("A1100");
+			account.setAcctId("221-22-3477");
+			account.deposit(100000);
+			
+			System.out.println("고객번호= " + account.getCustId());
+			System.out.println("계좌번호= " + account.getAcctId());
+			System.out.println("잔액= " + account.getBalance());
+			
+			
+		}catch(InsufficientBalanceException exp) {
+			System.out.println("Error 메시지=" + exp.getMessage() + exp.getCurrentBalance());
+		}
+	}
+}
--- src/workshop/account/entity/Account.java
+++ src/workshop/account/entity/Account.java
@@ -1,9 +1,24 @@
 package workshop.account.entity;
 
+import workshop.account.exception.InsufficientBalanceException;
+
 public class Account {
 	private String custId;
 	private String acctId;
 	private int  balance;
+	
+	//default constructor 선언
+	public Account() {
+		System.out.println("Account 기본생성자가 호출됨!");
+	}
+	//생성자 중복정의(constructor overloading)
+	public Account(String custId, String acctId, int balance) {
+		System.out.println("오버로딩된 생성자 호출됨!");
+		this.custId = custId;
+		this.acctId = acctId;
+		this.balance = balance;
+	}
+
 	
 	//getter method
 	public String getCustId() {
@@ -32,11 +47,22 @@ public class Account {
 		this.balance += amount;
 	}
 	//출금
-	public void withdraw(int amount) {
+	public void withdraw(int amount) 
+			throws InsufficientBalanceException {
+		if(amount >balance) {
+				//Exception을 강제로 발생시킴
+			throw new InsufficientBalanceException("잔액이 부족합니다, 현재 잔액은=", this.balance);
+		}
 		this.balance -= amount;
 	}
+	@Override
+	public String toString() {
+	    return "Account [고객번호=" + custId + ", "
+	            + "계좌번호=" + acctId + ", "
+	            + "잔액=" + balance + "]";
+	}
 	
 	
 	
 	}
-}
+
